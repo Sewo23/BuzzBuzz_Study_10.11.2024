69e9d4155e5ff245701b7fc46d85fdf49944d7c0
Sewo23 <seweryn.stoklosa@gmail.com>
2024-11-10 14:53:24 +0100

    Add files via upload

---
+++ Turtle_10.11.2024.py
@@ -0,0 +1,26 @@
+import turtle
+
+t = turtle.Pen()
+t.speed(0)
+t.shape("turtle")
+t.width(3)
+turtle.bgcolor('black')
+
+for i in range(36):
+    t.forward(300)
+    t.color('yellow')
+    t.right(90)
+    t.circle(10)
+    t.left(90)
+    t.color ('orange')
+    t.setpos(0,0)
+    t.left(10)
+
+colors = ["chartreuse", "magenta", "deepskyblue", "red"]
+
+for i in range (30):
+    t.color(colors[i % len(colors) ])
+    t.up()
+    t.setpos(0,-i * 10)
+    t.down()
+    t.circle(i * 10)
